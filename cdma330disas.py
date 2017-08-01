7d3120ccd68710cc3de60e3703cc1fd213e45967
TuxSH <tuxsh@sfr.fr>
2017-08-02 00:44:05 +0200

    Push code

---
+++ cdma330disas.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# Requires Python >= 3.2 or >= 2.7
+
+__author__    = "TuxSH"
+__copyright__ = "Copyright (c) 2017 TuxSH"
+__license__   = "GPLv3"
+__version__   = "v0.1"
+
+"""
+Corelink DMA-330 disassembler
+"""
+
+import argparse
+from struct import *
+def decodeInstruction(buf, off):
+    b = unpack_from("<B", buf, off)[0]
+    if (b & ~2) == 0x54:
+        reg = "DAR" if (b & 1) else "SAR"
+        imm = unpack_from("<H", buf, off + 1)[0]
+        return off + 3, "{0:10}{1}, #0x{2:X}".format("DMADDH", reg, imm)
+    elif (b & ~2) == 0x5C:
+        reg = "DAR" if (b & 1) else "SAR"
+        imm = unpack_from("<H", buf, off + 1)[0]
+        return off + 3, "{0:10}{1}, #0x{2:X}".format("DMADNH", reg, imm)
+    elif b == 0:
+        return off + 1, "DMAEND"
+    elif b == 0x35:
+        b2 = unpack_from("<B", buf, off + 1)[0]
+        if (b2 & 7) == 0:
+            return off + 2, "{0:10}0x{1:X}".format("DMAFLUSHP", b2 >> 3)
+        else:
+            return off + 2, "<invalid>"
+    elif (b & ~2) == 0xA0:
+        b2 = unpack_from("<B", buf, off + 1)
+        if (b2 & 7) == 0:
+            secure = ", ns" if (b & 1) else ""
+            chan = "C{0}".format(b2 & 7)
+            imm = unpack_from("<I", buf, off + 2)[0]
+            return off + 6, "{0:10}{1}, 0x{1:08X}{2}".format("DMAGO", chan, imm, secure)
+        else:
+                return off + 2, "<invalid>"
+    elif b == 1:
+        return off + 1, "DMAKILL"
+    elif (b & ~3) == 4:
+        kind = ("", "S", "<invalid>", "B")[b & 3]
+        return off + 1, "DMALD"+kind
+    elif (b & ~2) == 0x25:
+        kind = "B" if (b & 2) else "S"
+        b2 = unpack_from("<B", buf, off + 1)[0]
+        if (b2 & 7) == 0:
+            return off + 2, "{0:10}0x{1:X}".format("DMALDP"+kind, b2 >> 3)
+        else:
+            return off + 2, "<invalid>"
+    elif (b & ~2) == 0x20:
+        b2 = unpack_from("<B", buf, off + 1)[0]
+        return off + 2, "{0:10}0x{1:X}, {2}".format("DMALP", b2 + 1, (b & 2) >> 1)
+    elif (b & ~0x17) == 0x28:
+        b2 = unpack_from("<B", buf, off + 1)[0]
+        kind = ("", "S", "<invalid>", "B")[b & 3]
+        nf = "" if ((b & 0x10) >> 4) else ", FE"
+        return off + 2, "{0:10}{1:08X}, {2}{3}".format("DMALPEND"+kind, off - b2, (b & 4) >> 2, nf)
+    elif b == 0xBC:
+        b2 = unpack_from("<B", buf, off + 1)[0]
+        if (b2 & ~7) == 0:
+            reg = ("SAR", "CCR", "DAR", "<invalid>", "<invalid>", "<invalid>", "<invalid>", "<invalid>")[b2 & 3]
+            imm = unpack_from("<I", buf, off + 2)[0]
+            return off + 6, "{0:10}{1}, #0x{2:08X}".format("DMAMOV", reg, imm)
+        else:
+            return off + 2, "<invalid>"
+    elif b == 0x18:
+        return off + 1, "DMANOP"
+    elif b == 0x12:
+        return off + 1, "DMARMB"
+    elif b == 0x34:
+        b2 = unpack_from("<B", buf, off + 1)[0]
+        if (b2 & 7) == 0:
+            return off + 2, "{0:10}0x{1:X}".format("DMASEV", b2 >> 3)
+        else:
+            return off + 2, "<invalid>"
+    elif (b & ~3) == 8:
+        kind = ("", "S", "<invalid>", "B")[b & 3]
+        return off + 1, "DMAST"+kind
+    elif (b & ~2) == 0x29:
+        kind = "B" if (b & 2) else "S"
+        b2 = unpack_from("<B", buf, off + 1)[0]
+        if (b2 & 7) == 0:
+            return off + 2, "{0:10}0x{1:X}".format("DMASTP"+kind, b2 >> 3)
+        else:
+            return off + 2, "<invalid>"
+    elif b == 0xC:
+        return off + 1, "DMASTZ"
+    elif b == 0x36:
+        b2 = unpack_from("<B", buf, off + 1)[0]
+        if (b2 & 5) == 0:
+            inv = ", invalid" if (b2 & 2) else ""
+            return off + 2, "{0:10}0x{1:X}{2}".format("DMAWFE", b2 >> 3, inv)
+        else:
+            return off + 2, "<invalid>"
+    elif (b & ~3) == 0x30:
+        kind = ("single", "periph", "burst", "<invalid>")[b & 3]
+        b2 = unpack_from("<B", buf, off + 1)[0]
+        if (b2 & 7) == 0:
+            return off + 2, "{0:10}0x{1:X}, {2}".format("DMAWFP", b2 >> 3, kind)
+        else:
+            return off + 2, "<invalid>"
+    elif b == 0x13:
+        return off + 1, "DMAWMB"
+    else:
+        return off + 1, "{0:10}0x{1:02X}".format(".DCB", b)
+
+def main(args=None):
+    parser = argparse.ArgumentParser(prog="cdma330disas", description="Corelink DMA-330 disassembler.")
+    parser.add_argument("infile", help="Input file", type=argparse.FileType("rb"))
+    parser.add_argument("-b", "--base-address", help="Base address", type=int, default=0)
+    args = parser.parse_args()
+
+    data = args.infile.read()
+    args.infile.close()
+    off = 0
+    while off < len(data):
+        newOff, instr = decodeInstruction(data, off)
+        print("{0:08X}:    {1}".format(off+args.base_address, instr))
+        off = newOff
+
+if __name__ == '__main__':
+    main()
